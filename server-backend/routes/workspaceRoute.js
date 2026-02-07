b09222319abd1356c35c67399dfedc2edafc0da6
dishant334 <dishantk576@gmail.com>
2026-02-08 01:06:22 +0530

    Added Protected in routes

--- server-backend/routes/workspaceRoute.js
+++ server-backend/routes/workspaceRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { addWorkspace, getSingleWorkspace, getWorkspaces } from '../controllers/workspaceController.js'
+import protect from '../authMiddleWare/protect.js'
 
 const workspaceRouter=express.Router()
-workspaceRouter.post("/workspace", addWorkspace)
-workspaceRouter.get('/workspaces',getWorkspaces)
-workspaceRouter.get('/workspace/:workspaceid',getSingleWorkspace)
+workspaceRouter.post("/workspace",protect, addWorkspace)
+workspaceRouter.get('/workspaces',protect,getWorkspaces)
+workspaceRouter.get('/workspace/:workspaceid',protect,getSingleWorkspace)
 
 export default workspaceRouter
